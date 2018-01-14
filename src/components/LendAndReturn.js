e14fdfa03eee6ea31d13da25b687a69da886d70b
Joel <joel.salminen@gmail.com>
2018-01-14 23:51:16 +0200

    added default values to fields in LendAndReturn

--- src/components/LendAndReturn.js
+++ src/components/LendAndReturn.js
@@ -3,20 +3,63 @@ import React, { Component } from 'react';
 
 
 class LendAndReturn extends Component{
-	
+	constructor(props){
+		super(props)
+		this.state = {
+			user: "Test_user1",
+			date: "",
+			amount: 0
+		}
+		this.setDate = this.setDate.bind(this);
+		this.increaseByOne = this.increaseByOne.bind(this);
+		//this.decreaseByOne = this.decreaseByOne.bind(this);
+	}
+
+
+	componentWillMount(){
+		this.setDate();
+	}
+
+
+	increaseByOne(){
+		let newAmount = this.state.amount + 1;
+		this.setState({amount: newAmount});
+
+	}
+
+	// decreaseByOne(){
+	// 	let newAmount = this.state.amount -1;
+	// 	if (newAmount > 0){
+	// 	this.setState({amount: newAmount});
+	// 	}
+		
+	// }
+
+	setDate(){
+		const current = new Date();
+		const currentYear = current.getUTCFullYear();
+		const currentMonth = current.getUTCMonth() + 1;
+		const currentDay = current.getUTCDate();
+
+		const datex = "".concat(currentDay, ".", currentMonth, ".", currentYear);
+		this.setState({date: datex});
+	}
+
+
 	
 	render(){
+		console.log(this.state.amount);
 		return(
 		<div>
 			<Menu />
 			<button id="scan_button">Skannaa</button>
-			<input name="employee_name" placeholder="Tuotteen varastosta hakija" />
+			<input name="employee_name" placeholder="Tuotteen varastosta hakija" defaultValue={this.state.user}/>
 			<br/>
 			<input name="customer_name" placeholder="Asiakas" />
 			<br/>
 			<input name="customer_number" placeholder="Asiakkaan puhelinnumero" />
 			<br/>
-			<input name="current_date" placeholder="Päivämäärä tänään" />
+			<input name="current_date" placeholder="Päivämäärä tänään" defaultValue={this.state.date}/>
 			<br/>
 			<input name="expiration" placeholder="Palautuspäivämäärä" />
 			<br/>
@@ -29,8 +72,8 @@ class LendAndReturn extends Component{
 			<input name="serial_number" placeholder="Sarjanumero" />
 			<br/>
 			<button>-</button>
-			<input name="amount" placeholder="Lukumäärä" />
-			<button>+</button>
+			<input name="amount" placeholder="Lukumäärä" onChange={this.increaseByOne} value={this.state.amount}/>
+			<button onClick={this.increaseByOne} value={1}>+</button>
 			<br/>
 			<button>Lisää tuote</button>
 			<br/>
