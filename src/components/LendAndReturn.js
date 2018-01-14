5cc371212b3ee8b1aeaa7ddb6c1156dd70d2d6a0
Joel <joel.salminen@gmail.com>
2018-01-14 23:26:45 +0200

    Created basic structure for LendAndReturn page

--- src/components/LendAndReturn.js
+++ src/components/LendAndReturn.js
@@ -4,11 +4,48 @@ import React, { Component } from 'react';
 
 class LendAndReturn extends Component{
 	
+	
 	render(){
 		return(
 		<div>
 			<Menu />
-			<p>Lend</p>
+			<button id="scan_button">Skannaa</button>
+			<input name="employee_name" placeholder="Tuotteen varastosta hakija" />
+			<br/>
+			<input name="customer_name" placeholder="Asiakas" />
+			<br/>
+			<input name="customer_number" placeholder="Asiakkaan puhelinnumero" />
+			<br/>
+			<input name="current_date" placeholder="Päivämäärä tänään" />
+			<br/>
+			<input name="expiration" placeholder="Palautuspäivämäärä" />
+			<br/>
+
+
+			<br/>
+			<br/>
+			<input name="name" placeholder="Tuotteen nimi" />
+			<br/>
+			<input name="serial_number" placeholder="Sarjanumero" />
+			<br/>
+			<button>-</button>
+			<input name="amount" placeholder="Lukumäärä" />
+			<button>+</button>
+			<br/>
+			<button>Lisää tuote</button>
+			<br/>
+			<button>Poista tuote</button>
+			<br/>
+
+
+			<br/>
+			<input name="lending_type" placeholder="Lainauksen luonne" />
+			<br/>
+			<input name="price" placeholder="Hinta" />
+			<br/>
+
+			<button>Lisää</button>
+
 		</div>);
 	}
 
