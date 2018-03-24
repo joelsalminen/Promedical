fc0b117698462d6b4777fbf3ad7d8bc9814439bb
Joel <joel.salminen@gmail.com>
2018-03-24 15:27:52 +0200

    Serial number is now collected to state variable on Change

--- src/components/Return.js
+++ src/components/Return.js
@@ -4,18 +4,31 @@ import React, { Component } from 'react';
 
 
 class Return extends Component{
+	constructor(props){
+		super(props);
 
-	
+		this.state = {
+			serial: "",
+		}
+
+		this.serialChangeHandler = this.serialChangeHandler.bind(this);	
+	}
+
+
+	serialChangeHandler (evt){
+		this.setState({serial: evt.target.value });
+	}
+
 	render(){
-		
+
 
 		return(
 		<div>
 			<Menu />
-			<p>palautus</p>
+			<input name="serial_number" type="text" placeholder="serial number" onChange={this.serialChangeHandler}/>
+			<button>Palauta</button>
 
-
-
+			<p>{this.state.serial}</p>
 		</div>);
 	}
 
