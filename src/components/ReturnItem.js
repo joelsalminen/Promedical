df519a1fd448c99ce87fb7285ca7b8a57dd81c9f
Joel <joel.salminen@gmail.com>
2018-05-10 14:49:09 +0300

    Serial filtering now works

--- src/components/ReturnItem.js
+++ src/components/ReturnItem.js
@@ -9,11 +9,12 @@ class Return extends Component{
 
 		this.state = {
 			serial: "",
-			items: {}, 
+			items: {item: []}, 
 		}
 
 		this.serialChangeHandler = this.serialChangeHandler.bind(this);	
 		this.returnItem = this.returnItem.bind(this);
+		this.filterItems = this.filterItems.bind(this);
 	}
 
 
@@ -33,16 +34,33 @@ class Return extends Component{
 		
 	}
 
-	render(){
 
+	filterItems(items) {
+		
+		items = items.item.slice();
+		items = items.filter((item) => item.serial.toString().indexOf(this.state.serial) !== -1);
+
+		//items = items.filter(item => item.name.indexOf(this.state.nameSearch)!== -1);
+		if (this.state.serial === ""){
+			return [];	
+		}
+		return items;
+	}
+
+	render(){
+		let itemsList = this.filterItems(this.state.items);
+		
 		return(
 		<div>
 			<Menu />
 			<h1>Palautus</h1>
 
+			
 			<p>Sarjanumero:</p>
 			<input name="serial_number" type="text" placeholder="serial number" onChange={this.serialChangeHandler}/>
-
+			<ul>
+				{itemsList.map((item, index)=> <li key={index}>{item.name} {item.serial}</li> )}
+			</ul>
 			<br/>
 			<br/>
 			<button className="SubmitButton" onClick={this.returnItem}>Palauta</button>
