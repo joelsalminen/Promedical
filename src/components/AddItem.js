4f87262b83b3503457c2fbe3e91680a3314cfa26
Joel <joel.salminen@gmail.com>
2018-05-11 17:03:27 +0300

    serialChangeHandler added to AddItem

--- src/components/AddItem.js
+++ src/components/AddItem.js
@@ -6,10 +6,12 @@ class AddItem extends Component{
 	constructor(props){
 		super(props);
 		this.state = {
-			itemName: ""
+			itemName: "",
+			serial: "",
 		};
 		this.submitClickHandler = this.submitClickHandler.bind(this);
 		this.nameChangeHandler = this.nameChangeHandler.bind(this);
+		this.serialChangeHandler = this.serialChangeHandler.bind(this);
 	}
 
 	submitClickHandler(){
@@ -20,6 +22,10 @@ class AddItem extends Component{
 		this.setState({itemName: evt.target.value});
 	}
 
+	serialChangeHandler(evt){
+		this.setState({serial: evt.target.value});
+	}
+
 	render(){
 		return(
 
@@ -27,14 +33,14 @@ class AddItem extends Component{
 			<Menu />
 			<h1>Lisää tuote</h1>
 
-
+			<p>{this.state.serial}</p>
 			<button id="scan_button">Skannaa</button>
 			<p>Tuote</p>
 
 			<input name="name" placeholder="Tuotteen nimi" onChange={this.nameChangeHandler}/>
 			<p>Sarjanumero</p>
 
-			<input name="serial_number" placeholder="Sarjanumero" />
+			<input name="serial_number" placeholder="Sarjanumero" onChange={this.serialChangeHandler}/>
 			<p>Lukumäärä</p>
 
 			<input name="amount" placeholder="Lukumäärä" />
