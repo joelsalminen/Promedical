30ee46d95fd6720d4fe63501907b6b0e680f5fa6
joelsalminen <joel.salminen@gmail.com>
2018-05-26 15:18:27 +0300

    Edit button functionality added: cycles through editing state and regular state

--- src/components/StorageComponents/StorageListItem.js
+++ src/components/StorageComponents/StorageListItem.js
@@ -16,18 +16,35 @@ class StorageListItem extends Component{
 	}
 
 	onEditClick = ()=>{
-		this.props.editItem(this.props.item);
+		this.setState({isEditing: true});
+	}
+
+	onSaveClick = ()=>{
+		this.setState({isEditing: false})
+	}
+
+	onCancelClick = ()=>{
+		this.setState({isEditing: false})
 	}
 
 	renderItems(){
-		// items that are in storage
+
+		/* Editing state */
+		if (this.state.isEditing === true){
+			return (
+				<li key={this.props.item._id}>{this.props.item.name} - {this.props.item.serial} <button onClick={this.onSaveClick}>Tallenna</button><button onClick={this.onCancelClick}>Peruuta</button></li>
+			);
+		}
+
+		/* Regular state */
+		/* items that are in storage */
 		if (this.props.item.inStorage === true){
 			return(
 				<li key={this.props.item._id}>{this.props.item.name} - {this.props.item.serial} <button onClick={this.onEditClick}>Muokkaa</button><button onClick={this.onDeleteClick}>Poista</button></li>
 			);
 		}
 
-		// items that are with customers
+		/* items that are with customers */
 		else {
 			return(
 				<li className="NotInStorage" key={this.props.item._id}> {this.props.item.name} - {this.props.item.serial} - {this.props.item.customer} <button onClick={this.onEditClick}>Muokkaa</button></li>
