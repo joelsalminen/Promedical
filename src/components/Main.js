5182e025b7530a2c2a9d2e6ba429a5b923087155
joelsalminen <joel.salminen@gmail.com>
2018-05-30 16:56:01 +0300

    LoginForm and RegistrationForm added.

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 import NavBar from "./MainComponents/NavBar";
 import '../App.css';
+import LoginForm from "./MainComponents/LoginForm";
+import RegistrationForm from "./MainComponents/RegistrationForm";
 
 class Main extends Component{
 	render(){
 		return(
 		<div>
+			<LoginForm />
+			<RegistrationForm/>
 			<NavBar/>
 		</div>);
 	}
---
+++ src/components/MainComponents/RegistrationForm.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class RegistrationForm extends Component{
+	render(){
+		return(
+			<p>Registration</p>
+		);
+	}
+}
+
+export default RegistrationForm;
