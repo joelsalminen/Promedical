84bce2a88570497422086ffd20593b6fead5305f
joelsalminen <joel.salminen@gmail.com>
2018-07-01 11:27:45 +0300

    New Document view added.

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Storage from './components/Storage';
 import ReserveItem from './components/ReserveItem';
 //import History from './components/History';
 import ReturnItem from './components/ReturnItem';
+import Documents from './components/Documents.js'
 
 import './Reset.css';
 import './App.css';
@@ -32,6 +33,7 @@ class App extends Component {
         <Route exact path="/palautus" render={()=> <ReturnItem />}/>
         <Route exact path="/lisaatuote" component={AddItem}/>
         <Route exact path="/varasto" render={()=> <Storage />}/>
+        <Route exact path="/dokumentit" render={()=> <Documents />}/>
         {/* <Route exact path="/historia" component={History}/> */}
 
       </div>
---
+++ src/components/Documents.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Documents extends Component {
+	render(){
+		return(
+			<div>Documents</div>
+		);
+	}
+}
+
+export default Documents;
